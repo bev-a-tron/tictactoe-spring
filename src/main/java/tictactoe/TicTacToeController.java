72fbdb7f67d8ff6b14ea1235d70a795194525942
Kyle Olivo <kyleolivo@gmail.com>
2014-04-22 15:08:39 -0700

    Kyle & Eliza: Created tic tac toe board HTML page

---
+++ src/main/java/tictactoe/TicTacToeController.java
@@ -0,0 +1,14 @@
+package tictactoe;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TicTacToeController {
+
+    @RequestMapping("/")
+    public String showBoard(){
+        return "tictactoe";
+    }
+
+}
