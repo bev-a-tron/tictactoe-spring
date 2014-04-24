7fb2fdb549b5afed5455ae35e44fe1e95ce73137
Beverly Lau <beverly.a.lau@gmail.com>
2014-04-23 17:44:00 -0700

    Kyle & Bev | added JBehave libraries

---
+++ src/test/java/com/springapp/mvc/SimpleJBehave.java
@@ -0,0 +1,29 @@
+package com.springapp.mvc;
+
+import de.codecentric.jbehave.junit.monitoring.JUnitReportingRunner;
+import org.jbehave.core.junit.JUnitStories;
+import org.jbehave.core.steps.InjectableStepsFactory;
+import org.jbehave.core.steps.InstanceStepsFactory;
+import org.junit.runner.RunWith;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RunWith(JUnitReportingRunner.class)
+public class SimpleJBehave extends JUnitStories {
+
+    public SimpleJBehave() {
+        super();
+    }
+
+    @Override
+    public InjectableStepsFactory stepsFactory() {
+        return new InstanceStepsFactory(configuration(), new ExampleSteps());
+    }
+
+    @Override
+    public List<String> storyPaths() {
+        return Arrays.asList("com/springapp/mvc/Math.story");
+    }
+
+}
