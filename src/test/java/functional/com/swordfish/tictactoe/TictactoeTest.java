b91021224464838ddcc96a92813fb60c6c610e5d
Beverly Lau <beverly.a.lau@gmail.com>
2014-04-29 09:47:56 -0700

    Kyle, Bev and Eliza | Deleted MySeleniumTest, TicTacToeTest looks for the board on the page

--- src/test/java/functional/com/swordfish/tictactoe/TictactoeTest.java
+++ src/test/java/functional/com/swordfish/tictactoe/TictactoeTest.java
@@ -1,8 +1,13 @@
 package functional.com.swordfish.tictactoe;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.hamcrest.CoreMatchers.is;
 
 public class TictactoeTest {
 
@@ -10,10 +15,9 @@ public class TictactoeTest {
     public void shouldDrawBoard() {
         WebDriver driver = new FirefoxDriver();
         driver.navigate().to("localhost:8080/");
-//          Beverly | working on writing test for #1 Draw Board
-//        Assert.assertTrue("board should be drawn",
-//                driver.getTitle().startsWith("Selenium Simplified"));
-//        driver.close();
-//        driver.quit();
+        WebElement element = driver.findElement(By.id("game-board"));
+        Assert.assertThat(element.getAttribute("id"), is("game-board"));
+        driver.close();
+        driver.quit();
     }
 }
